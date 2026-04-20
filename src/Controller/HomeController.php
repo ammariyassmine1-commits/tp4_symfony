695ab5ca4cb2802c7248b7aa8707a14e323ef316
ammariyassmine1-commits <ammariyassmine1@gmail.com>
2026-04-20 18:01:35 -0400

    feat: Sessions - compteur de visites

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,23 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(RequestStack $requestStack): Response
     {
+        $session = $requestStack->getSession();
+
+        $nbVisites = $session->get('nb_visites', 0);
+        $session->set('nb_visites', $nbVisites + 1);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'nb_visites' => $nbVisites + 1,
         ]);
     }
 }
