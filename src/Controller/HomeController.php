c790fbd61bd004ff2618a843a3848050f43cdc97
ammariyassmine1-commits <ammariyassmine1@gmail.com>
2026-04-20 18:27:59 -0400

    feat: QueryBuilder - findLastPublished

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -9,15 +10,17 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(RequestStack $requestStack): Response
+    public function index(RequestStack $requestStack, ArticleRepository $articleRepository): Response
     {
         $session = $requestStack->getSession();
-
         $nbVisites = $session->get('nb_visites', 0);
         $session->set('nb_visites', $nbVisites + 1);
 
+        $derniersArticles = $articleRepository->findLastPublished(3);
+
         return $this->render('home/index.html.twig', [
             'nb_visites' => $nbVisites + 1,
+            'articles' => $derniersArticles,
         ]);
     }
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column]
+    private ?bool $publie = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function isPublie(): ?bool
+    {
+        return $this->publie;
+    }
+
+    public function setPublie(bool $publie): static
+    {
+        $this->publie = $publie;
+
+        return $this;
+    }
+}
